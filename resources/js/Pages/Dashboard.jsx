25022f96fd719f4dd0e89d62d4fe4b9ddde0e98d
arifrzkyfr <arifrzkyfr@gmail.com>
2026-05-13 18:42:11 +0700

    skripsi

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+export default function Dashboard() {
+
+    return (
+        <div style={{
+            background: "#0f172a",
+            color: "white",
+            minHeight: "100vh",
+            padding: "40px"
+        }}>
+            <h1>Smart Glove Dashboard</h1>
+
+            <p>
+                Sistem Monitoring Smart Glove IoT
+            </p>
+        </div>
+    )
+
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+
+    @inertiaHead
+
+</head>
+
+<body>
+
+    @inertia
+
+</body>
+</html>
