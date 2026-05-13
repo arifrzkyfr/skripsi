4768c96642925ad19e828d25ee89f758b3847933
arifrzkyfr <arifrzkyfr@gmail.com>
2026-05-13 19:00:18 +0700

    update : database

---
+++ database/migrations/2026_05_13_115622_create_gestures_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gestures', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->integer('f1');
+            $table->integer('f2');
+
+            $table->integer('ax');
+            $table->integer('ay');
+
+            $table->string('label');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gestures');
+    }
+};
